b55575ec979e4bfc9a730c1c52967dd7fda12b54
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-03-20 20:36:19 +0100

    remove old publishing

--- database-simplified-kit/build.gradle.kts
+++ database-simplified-kit/build.gradle.kts
@@ -16,24 +16,6 @@ signing {
     sign(configurations.archives.get())
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/avionik-world/database-simplified")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
-        }
-    }
-}
-
 centralPortal {
     username = project.findProperty("sonatypeUsername") as String
     password = project.findProperty("sonatypePassword") as String
