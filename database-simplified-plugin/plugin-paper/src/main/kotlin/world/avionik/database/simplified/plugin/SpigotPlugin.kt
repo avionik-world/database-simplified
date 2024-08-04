098acd15e0a99a86b87ba1336c3ce61f874d2d6f
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-08-04 15:02:49 +0200

    rename to plugin.yml

--- database-simplified-plugin/plugin-paper/src/main/kotlin/world/avionik/database/simplified/plugin/SpigotPlugin.kt
+++ database-simplified-plugin/plugin-paper/src/main/kotlin/world/avionik/database/simplified/plugin/SpigotPlugin.kt
@@ -1,13 +1,14 @@
 package world.avionik.database.simplified.plugin
 
 import org.bukkit.plugin.java.JavaPlugin
+import world.avionik.database.simplified.DatabaseSimplifiedKit
 import world.avionik.database.simplified.shared.DatabaseSimplified
 
 /**
  * @author Niklas Nieberler
  */
 
-class PaperPlugin : JavaPlugin() {
+class SpigotPlugin : JavaPlugin() {
 
     override fun onLoad() {
         DatabaseSimplified.create()
